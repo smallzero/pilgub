2d244cee05225af6ecec9f9a56583030295ca6f4
Agus Rochmad Tri Raharjo <abumamail@gmail.com>
2019-04-09 19:09:56 +0700

    add public version

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Pemiliah Gurbenur Jawa Timur
 1. Aplikasi bekerja menggunakan SMS Gateway. Pada database SMS inbox dibuat trigger untuk dilakukan proses parsing data. Data hasil parsing di-insert ke Database Pacitan pada table hasil suara. 
 2. Aplikasi menggunakan PHP untuk backend, CSS(Bootstrap) dan Javascript(JQuery)
 
+## Public Version
+1. Public Version ini adalah file yang diupload di server public.
+2. Keunggulannya adalah performance sangat tinggi dan sangat mudah untuk scale up performance.
+3. Tidak menggunakan database. Data backend menggunakan format JSON hasil upload dari server utama(SMS Gateway)
+4. Data diupload menggunakan scheduller. Ada di folder "command"(Windows)
+
---
+++ public_version/pilgub_pub/upload_pilgub.php
@@ -0,0 +1,29 @@
+<?php
+
+
+$target_dir = "backend/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+echo $fileType;
+
+    if($fileType=="json") {
+        echo "JSON";
+        $uploadOk = 1;
+    } else {
+        echo "FAILED";
+        $uploadOk = 0;
+    }
+	
+	if ($uploadOk == 0) {
+		echo "Sorry, your file was not uploaded.";
+	// if everything is ok, try to upload file
+	} else {
+		if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+			echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+		} else {
+			echo "Sorry, there was an error uploading your file.";
+		}
+	}
+
+?>
